c7a7b9cc5c058e94952392d04ab42712e04facb1
LeeJaeBin <2jaebin@gmail.com>
2026-09-04 17:12:58 +0900

    feat : 날씨조회 크롬으로 선회하도록 프롬프트 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.mystar.agent"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.12"
+        versionCode = 4
+        versionName = "0.13"
 
         buildConfigField("String", "LLM_API_KEY", "\"${escapeBuildConfig(llmApiKey)}\"")
         buildConfigField("String", "LLM_BASE_URL", "\"${escapeBuildConfig(llmBaseUrl)}\"")
